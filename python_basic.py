c48d0893c3313176a9d1dd98588065acf799cf93
Chaitanya Vura <vurachaitanya@users.noreply.github.com>
2020-03-07 01:45:47 +0530

    updated the code

---
+++ python_basic.py
@@ -0,0 +1,24 @@
+# Python
+
+## Sample program with if statement:
+age=23
+if age<18:
+  print("you are underage")
+elif age>18 and age<35:
+  print("you are young")
+else:
+  print("you are old")
+
+## Data types in python:
+x=5;type(x)
+y=1.5;type(y)
+s="hi";type(s)
+t=True;type(t)
+e=4+9j;type(e)
+f=[12,5,"hi"];type(f)
+f[-1];f[0]
+f.append(19)
+f.insert(3,5)
+f.remove(19)
+f
+ye=(12,4,6,8,"python",12.4);type(ye)
